1e51999c213195a5550fbce39be1956ae2871ec7
mahirkursun <mahirkrsn@gmail.com>
2021-10-19 00:02:03 +0300

    18 Ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,16 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+console.log(dolarDun)
+
+let konutKredileri = ["Konut Kredisi","Emlat Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log(konutKredileri)
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
